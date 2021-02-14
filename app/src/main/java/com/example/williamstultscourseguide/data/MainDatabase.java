8b5c3573d5143716594385d388388ef27e4eca2c
dogmatica <persona@autodogma.com>
2021-02-14 16:58:49 -0600

    Added basic MainDatabase class

---
+++ app/src/main/java/com/example/williamstultscourseguide/data/MainDatabase.java
@@ -0,0 +1,30 @@
+package com.example.williamstultscourseguide.data;
+
+import android.content.Context;
+
+import androidx.room.Dao;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
+
+@Database(entities = {Term.class, Course.class, Coursementor.class, Assessment.class}, exportSchema = false, version = 1)
+@TypeConverters({Converters.class})
+public abstract class MainDatabase extends RoomDatabase {
+
+    private static final String DB_NAME = "main_db";
+    private static MainDatabase instance;
+
+    public static synchronized MainDatabase getInstance(Context context) {
+        if (instance == null) {
+            instance = Room.databaseBuilder(context.getApplicationContext(), MainDatabase.class, DB_NAME).allowMainThreadQueries();
+        }
+        return instance;
+    }
+
+    public abstract TermDao termDao();
+    public abstract CourseDao courseDaoDao();
+    public abstract CoursementorDao coursementorDaoDao();
+    public abstract AssessmentDao assessmentDao();
+
+}
