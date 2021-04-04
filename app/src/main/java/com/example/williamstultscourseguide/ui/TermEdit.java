ea10c8db39d3bf2981e924867f8dabd6092114ec
dogmatica <persona@autodogma.com>
2021-04-04 15:57:53 -0500

    Created CourseEdit, TermEdit, and AssessmentEdit activities.

---
+++ app/src/main/java/com/example/williamstultscourseguide/ui/TermEdit.java
@@ -0,0 +1,24 @@
+package com.example.williamstultscourseguide.ui;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.williamstultscourseguide.R;
+
+public class TermEdit extends AppCompatActivity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_term_edit);
+
+        mTextView = (TextView) findViewById(R.id.text);
+
+        // Enables Always-on
+        //setAmbientEnabled();
+    }
+}
